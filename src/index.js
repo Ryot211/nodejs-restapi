2b0970a305d57faf93065074866b1ee8f9b35181
Bryan <gallarjoel@gmail.com>
2024-01-07 20:07:15 -0500

    cors1

--- src/index.js
+++ src/index.js
@@ -1,38 +1,27 @@
-import express from 'express'
-import carroRoutes from './routes/carro.routes.js'
-import indexRoutes from './routes/index.routes.js'
-
-
+import express from 'express';
 import cors from 'cors';
+import carroRoutes from './routes/carro.routes.js';
+import indexRoutes from './routes/index.routes.js';
+import { PORT } from './config.js';
 
-
+const app = express();
 
 const corsOptions = {
-  origin: ['https://nodejs-restapi-production-dfc4.up.railway.app/api/carros','http://127.0.0.1:500'], // Reemplaza esto con tu dominio de aplicación web
-  methods: ['GET', 'POST', 'PATCH','PUT', 'DELETE'], // Métodos permitidos
+  origin: ['https://nodejs-restapi-production-dfc4.up.railway.app', 'http://127.0.0.1:5000'], // Dominios permitidos
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
 
-// Resto de tu configuración de Express y definición de rutas...
+app.use(indexRoutes);
+app.use('/api', carroRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Endpoint no encontrado',
+  });
+});
 
-
-
-import {PORT} from './config.js'
-
-const app =express()
-
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api',carroRoutes)
-
-app.use((req, res , next ) =>{
-    res.status(404).json({
-        message:'endpoint Not found '
-    })
-} )
-
-app.listen(PORT)
+app.listen(PORT);
