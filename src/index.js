b4a981cab0cb30887cc81cdfb736019352e2c829
Bryan <gallarjoel@gmail.com>
2024-01-07 19:56:08 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import express from 'express'
 import carroRoutes from './routes/carro.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
+
+import cors from 'cors';
+
+
+
+const corsOptions = {
+  origin: 'https://nodejs-restapi-production-dfc4.up.railway.app/api/carros', // Reemplaza esto con tu dominio de aplicación web
+  methods: ['GET', 'POST', 'PATCH','PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+};
+
+app.use(cors(corsOptions));
+
+// Resto de tu configuración de Express y definición de rutas...
+
+
+
+
 import {PORT} from './config.js'
 
 const app =express()
