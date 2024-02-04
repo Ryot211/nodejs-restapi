cc55e1cfaaf07309fbbc6b2c9b1c605ab5298e14
Ryot211 <110674709+Ryot211@users.noreply.github.com>
2024-02-04 18:41:15 -0500

    act

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ import { config } from "dotenv";
 
 config()
  
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 3023
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '' 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_DATABASE = process.env.DB_DATABASE || 'carrosdb'
-export const DB_PORT = process.env.DB_PORT  || 3306
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'AGcEAhF5GfBd12Fefa-3f6Gdfhf4bHEC' 
+export const DB_HOST = process.env.DB_HOST || 'roundhouse.proxy.rlwy.net'
+export const DB_DATABASE = process.env.DB_DATABASE || 'railway'
+export const DB_PORT = process.env.DB_PORT  || 54121
